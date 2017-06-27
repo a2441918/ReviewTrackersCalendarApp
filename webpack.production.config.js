f93b0a073a5179ad7d95dfe4040e432bbb2163fc
Steve McKeogh <steve.mckeogh@gmail.com>
2017-06-27 10:10:25 +0100

    Production webpack config part 1

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,3 +1,79 @@
 /**
  * Production Webpack Configuration
  */
+
+let env = require('dotenv').config();
+let { resolve } = require('path');
+
+let webpack = require('webpack');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  
+  devtool: 'cheap-module-source-map',
+  
+  context: resolve(__dirname, 'src'),
+  
+  entry: [
+    './index.js',
+    //'./src/assets/scss/app.scss'
+  ],
+  
+  output: {
+    filename: 'app-[hash].js',
+    path: resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loaders: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[hash:base64:5]', 'postcss-loader', 'sass-loader'],
+        exclude: /node_modules/
+      },
+      { test: /\.(png|jpg|gif)$/, use: 'url-loader?limit=15000&name=assets/[hash:base64:5].[ext]' },
+      { test: /\.eot(\?v=\d+.\d+.\d+)?$/, use: 'file-loader' },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=image/svg+xml' }
+    ]
+  },
+  
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss']
+  },
+  
+  plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/src/index.html`,
+      filename: 'index.html',
+      inject: 'body',
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      mangle: {
+        screw_ie8: true,
+        keep_fnames: true,
+      },
+      compress: {
+        screw_ie8: true,
+      },
+      comments: false,
+    }),
+    new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } })
+  ]
+  
+}
