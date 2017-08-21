e8a6754710cf13e946c1a95e03008297a019b10e
Steve McKeogh <steve.mckeogh@gmail.com>
2017-08-21 14:35:56 +0100

    Redux added, siwth simple counter

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './store';
 
 import '../scss/app';
 
@@ -12,16 +14,18 @@ export default class App extends Component {
   
   render() {
     return (
-      <Router>
-        <div>
-          <Primary />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route component={NotFound}/>
-          </Switch>
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div>
+            <Primary />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route component={NotFound}/>
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
     );
   }
   
--- src/js/views/Home/index.jsx
+++ src/js/views/Home/index.jsx
@@ -4,22 +4,32 @@ import Alaska from '../../../img/alaska.jpg';
 
 import Styles from '../../../scss/views/home';
 
+import Counter from '../../components/Counter';
+
 export default class extends Component {
   
   render() {
     return (
       <div>
-        <h1>Home</h1>
-        <p>
-          <strong>Base64 data:image</strong><br />
+        <h1 style={{margin: '0 0 34px'}}>Home</h1>
+        <div style={{padding: '0 0 24px', margin: '0 0 24px', borderBottom: 'solid 2px #ddd'}}>
+          <strong>Redux counter</strong><br />
+          <Counter />
+        </div>
+        <div style={{padding: '0 0 24px', margin: '0 0 24px', borderBottom: 'solid 2px #ddd'}}>
+          <strong>Base64 data:image (under 10kb)</strong><br />
           <img src={Google} alt="Google" />
-        </p>
-        <p>
-          <strong>Standard src img!</strong><br />
+        </div>
+        <div style={{padding: '0 0 24px', margin: '0 0 24px', borderBottom: 'solid 2px #ddd'}}>
+          <strong>Standard src img (over 10kb)</strong><br />
           <img src={Alaska} width="300" alt="Alaska" />
-        </p>
-        <p className={Styles.module}>This is a css module class</p>
-        <p className="global">This is a global css class</p>
+        </div>
+        <div style={{padding: '0 0 24px', margin: '0 0 24px', borderBottom: 'solid 2px #ddd'}}>
+          <p className={Styles.module}>This is a css module class</p>
+        </div>
+        <div style={{padding: '0 0 24px', margin: '0 0 24px', borderBottom: 'solid 2px #ddd'}}>
+          <p className="global">This is a global css class</p>
+        </div>
       </div>
     )
   }
